bf7081994d7838b07743a3bb02bc2f665300fe7f
AgeenkoKonstantin <kostik2000ag@gmail.com>
2022-03-08 13:31:49 +0300

    Initial commit

---
+++ Ageenko_lab1/Data/Ageenko_lab1Context.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ageenko_lab1.Models;
+
+namespace Ageenko_lab1.Data
+{
+    public class Ageenko_lab1Context : DbContext
+    {
+        public Ageenko_lab1Context (DbContextOptions<Ageenko_lab1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ageenko_lab1.Models.Movie> Movie { get; set; }
+    }
+}
