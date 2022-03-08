c40e070329966e787b71626068d60a21838b994c
AgeenkoKonstantin <kostik2000ag@gmail.com>
2022-03-08 15:31:41 +0300

    part 5 done

--- Ageenko_lab1/Program.cs
+++ Ageenko_lab1/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Ageenko_lab1.Data;
+using Ageenko_lab1.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +18,13 @@ builder.Services.AddDbContext<Ageenko_lab1Context>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
